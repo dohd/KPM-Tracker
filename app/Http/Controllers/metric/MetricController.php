16a9f052b26302b6508c8fa3b64e129c6f106e64
Dohd <alloicebrian@gmail.com>
2026-01-16 08:59:39 +0300

    Fix: add year condition on checking duplicate meeting

--- app/Http/Controllers/metric/MetricController.php
+++ app/Http/Controllers/metric/MetricController.php
@@ -116,6 +116,7 @@ class MetricController extends Controller
             // duplicate meeting
             $is_exists = Metric::whereHas('programme', fn($q) => $q->where('metric', 'Online-Meeting'))
                 ->whereMonth('date', date('m', strtotime($input['date'])))
+                ->whereYear('date', date('Y', strtotime($input['date'])))
                 ->where(['programme_id' => $input['programme_id'], 'team_id' => $input['team_id']])
                 ->exists();
             if ($is_exists) return errorHandler('Metric input exists for a similar month');
@@ -214,6 +215,7 @@ class MetricController extends Controller
             $is_exists = Metric::where('id', '!=', $metric->id)
                 ->whereHas('programme', fn($q) => $q->where('metric', 'Online-Meeting'))
                 ->whereMonth('date', date('m', strtotime($input['date'])))
+                ->whereYear('date', date('Y', strtotime($input['date'])))
                 ->where(['programme_id' => $input['programme_id'], 'team_id' => $input['team_id']])
                 ->exists();
             if ($is_exists) return errorHandler('Metric input exists for a similar month');
